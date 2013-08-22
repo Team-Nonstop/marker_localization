742a064f25c81e0cd496c02195f3b28c2d2d3d98
KYI <kyi@ubuntu.(none)>
2013-08-22 19:06:34 +0900

    20130822

---
+++ include/marker_localization/marker_mark_data.hpp
@@ -0,0 +1,34 @@
+#include <sstream>
+#include <ros/ros.h>
+#include <yaml-cpp/yaml.h>
+
+#define VACANT  std::numeric_limits<unsigned int>::max()
+
+class MarkerMarkData
+{
+public:
+  class MarkData
+  {
+  public:
+    unsigned int           idx;          /**< Index; assigned according to the order on YAML file */
+    std::string            marker_id;
+    std::string            marker_link;   /**< Short description (optional) */
+
+    MarkData(unsigned int idx) : idx(idx) {};
+
+    void operator << (const YAML::Node& node);
+  };
+
+  MarkerMarkData() : allowed(VACANT) { }
+  ~MarkerMarkData() { }
+
+  std::vector<MarkData>::size_type size() { return list.size(); };
+  MarkData& operator [] (unsigned int idx) { return list[idx]; };
+
+  void configure(const YAML::Node& node);
+
+  unsigned int allowed;
+
+private:
+  std::vector<MarkData> list;
+};
