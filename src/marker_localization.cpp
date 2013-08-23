77527ee884cc7990e3fcbd98094061d864da12a9
KYI <kyi@ubuntu.(none)>
2013-08-23 12:14:15 +0900

    re-upload

--- src/marker_localization.cpp
+++ src/marker_localization.cpp
@@ -60,8 +60,6 @@ void matchParam(int marker_id, std::string &marker_frameid, std::string &target_
   }
 }
 
-<<<<<<< HEAD
-
 void ReceiveCallback(const visualization_msgs::Marker mrk)
 {
 
@@ -108,7 +106,6 @@ void ReceiveCallback(const visualization_msgs::Marker mrk)
   }
 
 
-
   tf::Transform transform;
   transform.setOrigin( map_to_target_base_link_transform.getOrigin() );
   transform.setRotation( map_to_target_base_link_transform.getRotation() );
@@ -117,66 +114,10 @@ void ReceiveCallback(const visualization_msgs::Marker mrk)
 
   //      posewcov_pub.publish(base_pose_withCov);
   //      pose_pub.publish(cam_base_pose);
-=======
-
-void ReceiveCallback(const visualization_msgs::Marker mrk)
-{
-
-  std::string marker_frameid = "/softstroller/marker_front_link";
-  std::string target_base_link_frame_id = "/softstroller/base_link";
-  std::string map_frameid = "/map";
-  std::string camera_frameid = "/watcher/camera_front";
-  
-		tf::StampedTransform map_to_camera_transform;
-		tf::StampedTransform camera_to_marker_transform;
-		tf::StampedTransform marker_to_target_base_link_transform;
-		tf::Transform map_to_target_base_link_transform ;
-
-		tf::Transform marker_transform;
-  marker_transform.setOrigin( tf::Vector3(mrk.pose.position.x, mrk.pose.position.y, mrk.pose.position.z)  );
-  marker_transform.setRotation( tf::Quaternion(mrk.pose.orientation.x,mrk.pose.orientation.y,mrk.pose.orientation.z,mrk.pose.orientation.w) );
-	switch(mrk.id){
-		// Should be replaced by due to parameter's value
-	}
-
-
-	try
-	{
-
-ROS_WARN("GET MARKERS %d",mr);
-		m_tfListener->lookupTransform(map_frameid, camera_frameid, ros::Time(), map_to_camera_transform);
-		m_tfListener->lookupTransform(marker_frameid, target_base_link_frame_id, ros::Time(), marker_to_target_base_link_transform);
-		camera_to_marker_transform = tf::StampedTransform(marker_transform, ros::Time::now(), camera_frameid, marker_frameid);
-
-
-		map_to_target_base_link_transform = map_to_camera_transform*camera_to_marker_transform*marker_to_target_base_link_transform
-		;
-		
-		
-
-	}
-	catch(tf::TransformException &e)
-	{
-		ROS_ERROR("Failed to transform");
-		return;
-	}
-
-
-
- tf::Transform transform;
-  transform.setOrigin( map_to_target_base_link_transform.getOrigin() );
-  transform.setRotation( map_to_target_base_link_transform.getRotation() );
-
-	m_tfBroadcaster->sendTransform(tf::StampedTransform(transform, ros::Time::now(), map_frameid, "/stroller"));
-
-//	posewcov_pub.publish(base_pose_withCov);
-//	pose_pub.publish(cam_base_pose);
->>>>>>> 03ac4d7a923adcbf4206045b02cbf30b259e3e5d
 }
 
 int main(int argc, char** argv)
 {
-<<<<<<< HEAD
   ros::init(argc, argv, "marker_localization");
   ros::NodeHandle n;
   ros::Rate loop_rate(100);
@@ -203,28 +144,4 @@ int main(int argc, char** argv)
       loop_rate.sleep();
     }
   return 0;
-=======
-	ros::init(argc, argv, "marker_localization");
-	ros::NodeHandle n;
-	ros::Rate loop_rate(100);
-
-	m_tfListener = new tf::TransformListener();
-	m_tfBroadcaster = new tf::TransformBroadcaster();
-
-//	posewcov_pub = n.advertise<geometry_msgs::PoseWithCovarianceStamped>("softstroller_pose", 1);
-//	pose_pub = n.advertise<geometry_msgs::PoseStamped>("softstroller_pose_simple", 1); // For monitoring
-	sub = n.subscribe("/visualization_marker", 1000, ReceiveCallback);
-//	float data_x=0.0;
-
-	while(ros::ok())
-	{
-//		base_pose_withCov.pose.covariance[0] = 0.25;
-//		base_pose_withCov.pose.covariance[7] = 0.25;
-//		base_pose_withCov.pose.covariance[35] = 0.06853891945200942;
-
-		ros::spinOnce();
-		loop_rate.sleep();
-	}
-	return 0;
->>>>>>> 03ac4d7a923adcbf4206045b02cbf30b259e3e5d
 }
